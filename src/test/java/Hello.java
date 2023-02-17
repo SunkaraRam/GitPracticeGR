fa3d8a486612986f223cffd96d65c5ed82287d1a
Sunkararam <sunkararam1166@gmail.com>
2023-02-17 16:46:04 +0530

    Added syso for Gowreesh

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,10 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		
+		System.out.println("This is Ramu");
+		
+		System.out.println("This is Gowreesh");
 		// TODO Auto-generated method stub
 
 	}
