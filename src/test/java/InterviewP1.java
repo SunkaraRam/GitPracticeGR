fd767549ad28305e82e1f62dcd7379e03d3550cc
Ganesh <ganeshyelamakuri@gmail.com>
2023-02-21 21:31:38 +0530

    new commit line added

--- src/test/java/InterviewP1.java
+++ src/test/java/InterviewP1.java
@@ -4,6 +4,7 @@ public class InterviewP1 {
 	public static void main(String[] args) {
 		
 		System.out.println("This is program1");
+		System.out.println(" program of the Interview class");
 	}
 
 }
